5ab62f8a113e6b55acf1e7195cfa0b52e95b3c4b
emilymliau <emily.m.liau@gmail.com>
2022-11-07 16:54:07 -0600

    Preliminary Volcano Plots

--- find_DEG.R
+++ find_DEG.R
@@ -1,11 +1,11 @@
 ######
 # LOAD PACKAGES
 ######
-library(dplyr)
-library(janitor)
+suppressMessages(library(dplyr))
+suppressMessages(library(janitor))
 
 ######
-# READ IN DATA
+# DATA PRE-PROCESSING
 ######
 luad_data <- read.table(file="LUAD_Differential_Gene_Expression_Table.txt", 
                    header=TRUE, sep="\t")
@@ -31,39 +31,99 @@ paad_data <- paad_data %>%
   clean_names()
 paad_p_val <- paad_data[paad_data$fdr_adjusted_p_value<=.05,]
 
-
-######
-# FIND COMMON GENES 
-######
-
 # remove cancer type column
 paad <- paad_p_val[-1]
 kirc <- kirc_p_val[-1]
 brca <- brca_p_val[-1]
 luad <- luad_p_val[-1]
 
+
+head(brca)
+head(kirc)
+head(luad)
+head(paad)
+
+
+# extracting relevant columns
+brcavp <- subset(brca, select = c('ncbi_gene_id', 'BRCA_fdr_adjusted_p_value', 'BRCA_log2_fold_change'))
+kircvp <- subset(kirc, select = c('ncbi_gene_id', 'KIRC_fdr_adjusted_p_value', 'KIRC_log2_fold_change'))
+luadvp <- subset(luad, select = c('ncbi_gene_id', 'LUAD_fdr_adjusted_p_value', 'LUAD_log2_fold_change'))
+paadvp <- subset(paad, select = c('ncbi_gene_id', 'PAAD_fdr_adjusted_p_value', 'PAAD_log2_fold_change'))
+
+View(brcavp)
+View(kircvp)
+View(luadvp)
+View(paadvp)
+
+######
+# VOLCANO PLOT
+######
+
+suppressMessages(library(ggplot2))
+
+# estatblish cutoff values
+# padj.cutoff <- 0.05
+# lfc.cutoff <- 0.58
+
+# BRCA Volcano Plot
+ggplot(data = brcavp, aes(x = BRCA_log2_fold_change, y = -log10(BRCA_fdr_adjusted_p_value))) + geom_point(size = 0.5) + coord_cartesian(xlim = c(-5, 5), ylim = c(0, 20)) + ggtitle("BRCA Volcano Plot")
+
+# KIRC Volcano Plot
+ggplot(data = kircvp, aes(x = KIRC_log2_fold_change, y = -log10(KIRC_fdr_adjusted_p_value))) + geom_point(size = 0.5) + coord_cartesian(xlim = c(-5, 5), ylim = c(0, 20)) + ggtitle("KIRC Volcano Plot")
+
+# LUAD Volcano Plot
+ggplot(data = luadvp, aes(x = LUAD_log2_fold_change, y = -log10(LUAD_fdr_adjusted_p_value))) + geom_point(size = 0.5) + coord_cartesian(xlim = c(-5, 5), ylim = c(0, 20)) + ggtitle("LUAD Volcano Plot")
+
+# PAAD Volcano Plot
+ggplot(data = paadvp, aes(x = PAAD_log2_fold_change, y = -log10(PAAD_fdr_adjusted_p_value))) + geom_point(size = 0.5) + coord_cartesian(xlim = c(-5, 5), ylim = c(0, 20)) + ggtitle("PAAD Volcano Plot")
+
+
+
+hist(brcavp$BRCA_fdr_adjusted_p_value, 
+     col="grey", border="white", xlab="", ylab="", main="BRCA - frequencies of adj. p-values\n(all genes)")
+
+hist(kircvp$KIRC_fdr_adjusted_p_value, 
+     col="grey", border="white", xlab="", ylab="", main="KIRC - frequencies of adj. p-values\n(all genes)")
+
+hist(luadvp$LUAD_fdr_adjusted_p_value, 
+     col="grey", border="white", xlab="", ylab="", main="LUAD - frequencies of adj. p-values\n(all genes)")
+
+hist(paadvp$PAAD_fdr_adjusted_p_value, 
+     col="grey", border="white", xlab="", ylab="", main="PAAD - frequencies of adj. p-values\n(all genes)")
+
+
+########################################################################
+########################################################################
+########################################################################
+
+
+
+######
+# FIND COMMON GENES 
+######
+
 # create unique column names for each cancer
-colnames(paad) <- c("ncbi_gene_id","PAAD_fdr_adjusted_p_value", "PAAD_cancer_sample_med",
+# colnames(paad) <- c("ncbi_gene_id","PAAD_fdr_adjusted_p_value", "PAAD_cancer_sample_med",
                     "PAAD_normal_sample_med", "PAAD_log2_fold_change", "PAAD_p_value", 
                     "gene_symbol")
-colnames(kirc) <- c("ncbi_gene_id","KIRC_fdr_adjusted_p_value", "KIRC_cancer_sample_med",
+# colnames(kirc) <- c("ncbi_gene_id","KIRC_fdr_adjusted_p_value", "KIRC_cancer_sample_med",
                     "KIRC_normal_sample_med", "KIRC_log2_fold_change", "KIRC_p_value", 
                     "gene_symbol")
-colnames(brca) <- c("ncbi_gene_id","BRCA_fdr_adjusted_p_value", "BRCA_cancer_sample_med",
+# colnames(brca) <- c("ncbi_gene_id","BRCA_fdr_adjusted_p_value", "BRCA_cancer_sample_med",
                     "BRCA_normal_sample_med", "BRCA_log2_fold_change", "BRCA_p_value", 
                     "gene_symbol")
-colnames(luad) <- c("ncbi_gene_id","LUAD_fdr_adjusted_p_value", "LUAD_cancer_sample_med",
+# colnames(luad) <- c("ncbi_gene_id","LUAD_fdr_adjusted_p_value", "LUAD_cancer_sample_med",
                     "LUAD_normal_sample_med", "LUAD_log2_fold_change", "LUAD_p_value", 
                     "gene_symbol")
 
 # merge paad and kirc
-paad_kirc <- merge(paad, kirc, by=c("ncbi_gene_id", "gene_symbol"))
+# paad_kirc <- merge(paad, kirc, by=c("ncbi_gene_id", "gene_symbol"))
 # merge paad and kirc and brca
-paad_kirc_brca <- merge(paad_kirc, brca, by=c("ncbi_gene_id", "gene_symbol"))
+# paad_kirc_brca <- merge(paad_kirc, brca, by=c("ncbi_gene_id", "gene_symbol"))
 # paad and kirc and brca and luad
-all <- merge(paad_kirc_brca, luad, by=c("ncbi_gene_id", "gene_symbol"))
+# all <- merge(paad_kirc_brca, luad, by=c("ncbi_gene_id", "gene_symbol"))
 
-View(all)
+# View(all)
 
 # write data to the csv file
 #write.csv(all, "all.csv", row.names = FALSE)
